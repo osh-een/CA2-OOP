1380ccee6f4e4afc83980bb77a4527d81a166fb5
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-04 18:07:06 +0000

    Removed redundant package name in comment

--- CA2 Project/src/Question8.java
+++ CA2 Project/src/Question8.java
@@ -11,6 +11,6 @@ public class Question8 // Multi-company (Queue)
 
     public static void main(String[] args) {
 
-        // code integrated in q7andq8.Question7
+        // code integrated in Question7
     }
 }
