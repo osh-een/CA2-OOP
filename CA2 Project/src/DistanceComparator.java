57942069800fffbd1aaee437415caac4cdb444e1
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-05 19:48:10 +0000

    Added file and code for comparing distances

---
+++ CA2 Project/src/DistanceComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class DistanceComparator implements Comparator<DistanceTo> {
+    
+    @Override
+    public int compare(DistanceTo distance1, DistanceTo distance2)
+    {
+        return Integer.compare(distance1.getDistance(), distance2.getDistance());
+    }
+}
