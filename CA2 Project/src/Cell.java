121d4bec1574de2e8c77c1889335e80fb48bec9a
osh-een <oisinbell09@gmail.com>
2024-12-06 13:16:03 +0000

    Create Cell.java

---
+++ CA2 Project/src/Cell.java
@@ -0,0 +1,30 @@
+public class Cell {
+    private int x;
+    private int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+}
