b9d270a6f8c7d266d5048ad8a3b157cfa2bcd816
lareishim <laremgmt@gmail.com>
2024-12-06 10:47:24 +0000

    new

--- CA2 Project/src/Question11.java
+++ CA2 Project/src/Question11.java
@@ -11,6 +11,9 @@ import java.util.*;
     // 1. If there are two paths with the same distance, errors happen
     // 2. Can't backtrack ie. Princeton to Pendleton
 
+    // pathnames
+    //"C:\\Users\\larem\\Documents\\DKIT\\YEAR 2\\Computer Security\\CA2-OOP\\CA2 Project\\src\\cityconnections.txt"
+
 public class Question11 {
 
     static Scanner scanner = new Scanner(System.in);
@@ -51,43 +54,65 @@ public class Question11 {
         System.out.println("Where would you like to travel to?");
         String destination = validateCity();
 
-        // no matter what, cities with the shortest distance will always be first in the queue
+        // Priority Queue to store cities and their distances (sorted by distance)
         PriorityQueue<DistanceTo> distances = new PriorityQueue<>(new DistanceComparator());
+        // Map to store the shortest known distance to each city
         Map<String, Integer> shortestKnownDistances = new HashMap<>();
+        // Set to track visited cities, to avoid reprocessing them
+        Set<String> visitedCities = new HashSet<>();  // NEW: Track visited cities
 
+        // Add the starting city to the priority queue with distance 0
         distances.add(new DistanceTo(location, 0));
 
-        while(!distances.isEmpty()) {
+        while (!distances.isEmpty()) {
+            // Get the city with the smallest distance (priority queue will give the smallest distance first)
             DistanceTo currentDistanceTo = distances.remove();
             String currentCity = currentDistanceTo.getTarget();
             int shortestCurrentDistance = currentDistanceTo.getDistance();
 
-            // puts top element of PriorityQueue as shortest getDistance() have priority
-            shortestKnownDistances.put(currentCity, shortestCurrentDistance);
-
-            // break the code if we're at the intended city (destination)
-            if(currentCity.equals(destination)) {
-                System.out.println("The shortest distance from " +location+ " to " +destination+ " is " +shortestCurrentDistance+ ". Returning to main menu...\n");
-                menuOptions();
+            // NEW: Skip cities that have already been processed (i.e., cities with finalized distances)
+            if (visitedCities.contains(currentCity)) {
+                continue;
             }
 
-            for(DistanceTo connectedCity : connectionsMap.get(currentCity)) {
+            // Mark the current city as visited (finalized)
+            visitedCities.add(currentCity);
+
+            // Store the shortest known distance for the current city
+            shortestKnownDistances.put(currentCity, shortestCurrentDistance);
+
+            // If we have reached the destination, print the result and return to the main menu
+            if (currentCity.equals(destination)) {
+                System.out.println("The shortest distance from " + location + " to " + destination + " is " + shortestCurrentDistance + ". Returning to main menu...\n");
+                menuOptions();
+                return;  // Return to the main menu after printing the result
+            }
+
+            // Add all neighboring cities to the priority queue with updated distances
+            for (DistanceTo connectedCity : connectionsMap.get(currentCity)) {
                 String neighbour = connectedCity.getTarget();
                 int neighbourDistance = connectedCity.getDistance();
 
+                // Calculate the new distance to the neighboring city
                 int newDistance = shortestCurrentDistance + neighbourDistance;
 
-                // only add it to PriorityQueue if the city is not yet in Map or if newDistance is shorter than distance of current neighbour city
-                if (!shortestKnownDistances.containsKey(neighbour) || newDistance < shortestKnownDistances.get(neighbour)) {
+                // NEW: Only add the neighboring city to the priority queue if:
+                // 1. It has not been visited yet.
+                // 2. It has not been processed before or the new distance is shorter than its previous shortest distance.
+                if (!visitedCities.contains(neighbour) && (!shortestKnownDistances.containsKey(neighbour) || newDistance < shortestKnownDistances.get(neighbour))) {
                     distances.add(new DistanceTo(neighbour, newDistance));
                 }
             }
         }
+
+        // If we exit the loop, it means there is no path to the destination
+        System.out.println("No path found from " + location + " to " + destination + ". Returning to main menu...\n");
+        menuOptions();  // Return to the main menu if no path is found
     }
 
     public static void addAllConnectionsFromFile() {
         try {
-            File file = new File("/Users/user/Documents/GitHub/CA2-OOP/CA2 Project/src/cityconnections.txt");
+            File file = new File("C:\\Users\\larem\\Documents\\DKIT\\YEAR 2\\Computer Security\\CA2-OOP\\CA2 Project\\src\\cityconnections.txt");
             Scanner fileScanner = new Scanner(file);
 
             while(fileScanner.hasNext()) {
